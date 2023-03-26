ec1029ed1ff7b5201063d223060e71ebbd8f3ad1
MagiccRabbit <radekzobanik@seznam.cz>
2023-03-26 13:07:04 +0200

    Added foreign keys + Filled with some data

--- druha_uloha/xzoban02_xmecia00.sql
+++ druha_uloha/xzoban02_xmecia00.sql
@@ -10,14 +10,21 @@ DROP TABLE T_Hospitalization CASCADE CONSTRAINTS;
 DROP TABLE T_Clinic CASCADE CONSTRAINTS;
 DROP TABLE T_Medical_equipment CASCADE CONSTRAINTS;
 
+CREATE TABLE T_Clinic
+(
+    Clinic_name         VARCHAR(30) NOT NULL PRIMARY KEY,
+    Telephone           INTEGER
+);
+
 CREATE TABLE T_Medic
 (
     Medic_ID    INTEGER NOT NULL PRIMARY KEY,
-    First_name  VARCHAR(11),
-    Last_name   VARCHAR(11),
+    First_name  VARCHAR(20),
+    Last_name   VARCHAR(20),
     Clinic_name VARCHAR(30),
     Duty        VARCHAR(20),
-    Status      VARCHAR(20)
+    Status      VARCHAR(20),
+    FOREIGN KEY (Clinic_name) REFERENCES T_Clinic(Clinic_name)
 );
 
 CREATE TABLE T_Doctor
@@ -34,8 +41,8 @@ CREATE TABLE T_Nurse
 CREATE TABLE T_Patient
 (
     Personal_ID INTEGER NOT NULL PRIMARY KEY,
-    First_name  VARCHAR(11),
-    Last_name   VARCHAR(11)
+    First_name  VARCHAR(20),
+    Last_name   VARCHAR(20)
 );
 
 CREATE TABLE T_Examination
@@ -45,7 +52,9 @@ CREATE TABLE T_Examination
     Personal_ID INTEGER,
     Exam_Date   VARCHAR(10),
     Exam_Time   VARCHAR(5),
-    Location_ID INTEGER
+    Location_ID INTEGER,
+    FOREIGN KEY (Medic_ID) REFERENCES T_Medic(Medic_ID) ,
+    FOREIGN KEY (Personal_ID) REFERENCES T_Patient(Personal_ID)
 );
 
 CREATE TABLE T_Surgery
@@ -55,7 +64,9 @@ CREATE TABLE T_Surgery
     Personal_ID  INTEGER,
     Surgery_date VARCHAR(10),
     Surgery_Time VARCHAR(5),
-    Location_ID  INTEGER
+    Location_ID  INTEGER,
+    FOREIGN KEY (Medic_ID) REFERENCES T_Medic(Medic_ID) ,
+    FOREIGN KEY (Personal_ID) REFERENCES T_Patient(Personal_ID)
 );
 
 -- Identifies location, makes code cleaner
@@ -64,7 +75,8 @@ CREATE TABLE T_Location
     Location_ID INTEGER NOT NULL PRIMARY KEY,
     Clinic_name VARCHAR(30),
     Pavilion    VARCHAR(30),
-    Room        VARCHAR(30)
+    Room        VARCHAR(30),
+    FOREIGN KEY (Clinic_name) REFERENCES T_Clinic(Clinic_name) ON DELETE CASCADE
 );
 
 CREATE TABLE T_Prescribed_Drug
@@ -74,7 +86,9 @@ CREATE TABLE T_Prescribed_Drug
     Personal_ID       INTEGER,
     Prescription_date VARCHAR(10),
     Prescription_time VARCHAR(5),
-    Cure              VARCHAR(20)
+    Cure              VARCHAR(20),
+    FOREIGN KEY (Medic_ID) REFERENCES T_Medic(Medic_ID) ,
+    FOREIGN KEY (Personal_ID) REFERENCES T_Patient(Personal_ID)
 );
 
 CREATE TABLE T_Hospitalization
@@ -83,44 +97,109 @@ CREATE TABLE T_Hospitalization
     Medic_ID           INTEGER,
     Personal_ID        INTEGER,
     Date_from          varchar(10),
-    Date_till          varchar(10)
-);
-
-CREATE TABLE T_Clinic
-(
-    Clinic_name          INTEGER NOT NULL PRIMARY KEY,
-    Telephone            INTEGER,
-    Medical_equipment_ID INTEGER
+    Date_till          varchar(10),
+    FOREIGN KEY (Medic_ID) REFERENCES T_Medic(Medic_ID) ,
+    FOREIGN KEY (Personal_ID) REFERENCES T_Patient(Personal_ID)
 );
 
 CREATE TABLE T_Medical_equipment
 (
     Medical_equipment_ID INTEGER NOT NULL PRIMARY KEY,
+    Clinic_name          VARCHAR(30),
     Equipment            VARCHAR(30),
-    Amount               INTEGER
+    Amount               INTEGER,
+    FOREIGN KEY (Clinic_name) REFERENCES T_Clinic(Clinic_name) ON DELETE CASCADE
 );
 
+INSERT INTO T_Clinic
+VALUES ('Dětské','222333444');
+INSERT INTO T_Clinic
+VALUES ('Ambulance','528625362');
+INSERT INTO T_Clinic
+VALUES ('Neurologie','777777777');
 
--- Add Primary keys
--- ALTER TABLE T_Medic
---     ADD CONSTRAINT PK_Medic PRIMARY KEY (Medic_ID);
--- ALTER TABLE T_Doctor
---     ADD CONSTRAINT PK_Doctor PRIMARY KEY (Medic_ID);
--- ALTER TABLE T_Nurse
---     ADD CONSTRAINT PK_Nurse PRIMARY KEY (Medic_ID);
--- ALTER TABLE T_Patient
---     ADD CONSTRAINT PK_Patient PRIMARY KEY (Personal_ID);
--- ALTER TABLE T_Examination
---     ADD CONSTRAINT PK_PRIMARY KEY (Exam_ID);
--- ALTER TABLE T_Surgery
---     ADD CONSTRAINT PRIMARY KEY (Surgery_ID);
--- ALTER TABLE T_Location
---     ADD CONSTRAINT PRIMARY KEY (Location_ID);
--- ALTER TABLE T_Prescribed_Drug
---     ADD CONSTRAINT PRIMARY KEY (Drug_ID);
--- ALTER TABLE T_Hospitalization
---     ADD CONSTRAINT PRIMARY KEY (Hospitalization_ID);
--- ALTER TABLE T_Clinic
---     ADD CONSTRAINT PRIMARY KEY (Clinic_name);
--- ALTER TABLE T_Medical_equipment
---     ADD CONSTRAINT PRIMARY KEY (Medical_equipment_ID);
+INSERT INTO T_Medic
+VALUES ('1','Alena','Chytrá','Ambulance','plný','všeobecný');
+INSERT INTO T_Medic
+VALUES ('2','Anton','Stašek','Ambulance','plný','všeobecný');
+INSERT INTO T_Medic
+VALUES ('3','Michal','Novotný','Neurologie','plný','primář');
+INSERT INTO T_Medic
+VALUES ('4','Anna','Černá','Neurologie','plný','všeobecný');
+INSERT INTO T_Medic
+VALUES ('5','Dana','Loučková','Dětské','plný','všeobecný');
+INSERT INTO T_Medic
+VALUES ('6','David','Novák','Dětské','směny','pratik');
+
+INSERT INTO T_Doctor
+VALUES ('1','ambulantní');
+INSERT INTO T_Doctor
+VALUES ('2','ambulantní');
+
+INSERT INTO T_Nurse
+VALUES ('4');
+INSERT INTO T_Nurse
+VALUES ('5');
+
+INSERT INTO T_Patient
+VALUES ('9502146005','Evžen','Bezděk');
+INSERT INTO T_Patient
+VALUES ('9301049593','Dominik','Nevřela');
+INSERT INTO T_Patient
+VALUES ('7062012562','Eliška','Hoffmannová');
+
+INSERT INTO T_Medical_equipment
+VALUES ('42','Dětské','Váha','10');
+INSERT INTO T_Medical_equipment
+VALUES ('43','Ambulance','Postel','76');
+INSERT INTO T_Medical_equipment
+VALUES ('44','Neurologie','Rentgen','2');
+INSERT INTO T_Medical_equipment
+VALUES ('45','Dětské','Zraková_tabule','10');
+INSERT INTO T_Medical_equipment
+VALUES ('46','Ambulance','Povlečení','336');
+INSERT INTO T_Medical_equipment
+VALUES ('47','Neurologie','Magnetická_rezonance','1');
+
+INSERT INTO T_Location
+VALUES ('2413','Dětské','A','E21');
+INSERT INTO T_Location
+VALUES ('521','Ambulance','C','127');
+INSERT INTO T_Location
+VALUES ('15','Neurologie','A','11');
+INSERT INTO T_Location
+VALUES ('221','Neurologie','B','A12');
+
+INSERT INTO T_Prescribed_Drug
+VALUES ('222','1','9502146005','2022-06-22','15:01','diazepam');
+INSERT INTO T_Prescribed_Drug
+VALUES ('521','1','7062012562','2022-07-09','12:41','inzulin');
+INSERT INTO T_Prescribed_Drug
+VALUES ('634','2','7062012562','2022-08-13','21:35','diazepam');
+INSERT INTO T_Prescribed_Drug
+VALUES ('991','2','7062012562','2022-05-21','11:22','diazepam');
+
+
+
+INSERT INTO T_Examination
+VALUES ('11111','6','9502146005','2022-08-25','12:24','2431');
+INSERT INTO T_Examination
+VALUES ('12222','2','9301049593','2022-06-04','07:15','521');
+
+INSERT INTO T_Examination
+VALUES ('1','6','9502146005','2022-08-25','12:24','2431');
+INSERT INTO T_Examination
+VALUES ('2','2','9301049593','2022-06-04','07:15','521');
+
+INSERT INTO T_Surgery
+VALUES ('1','6','9502146005','2022-03-01','15:01','2431');
+INSERT INTO T_Surgery
+VALUES ('2','2','9301049593','2022-12-23','8:00','521');
+
+INSERT INTO T_Hospitalization
+VALUES ('99921','6','9502146005','2022-03-01','2022-03-09');
+INSERT INTO T_Hospitalization
+VALUES ('99922','2','9301049593','2022-12-23','2023-01-01');
+
+commit ;
+
